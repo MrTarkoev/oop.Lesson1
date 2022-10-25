70b47e8c8d75aa15f844772c1e3b0b57393c3d23
MrTarkoev <112986386+MrTarkoev@users.noreply.github.com>
2022-10-25 21:21:36 +0300

    Выполнил ДЗ ООП. Урок 1 (введение, object) Домашнее Задание 3. Задание 1, 2

--- src/Человек.java
+++ src/Человек.java
@@ -5,11 +5,30 @@ public class Человек {
     String jobTitle;
 
     Человек(String name, String town, int yearOfBirth, String jobTitle) {
-        this.yearOfBirth = yearOfBirth;
+
+        if (yearOfBirth >= 0) {
+            this.yearOfBirth = yearOfBirth;
+        } else {
+            this.yearOfBirth = Math.abs(yearOfBirth);
+        }
         this.town = town;
         this.name = name;
         this.jobTitle = jobTitle;
 
-
+        if  (name == null || name.isEmpty()) {
+            this.name = "Информация не указана!";
+        }else {
+            this.name = name;
+        }
+        if  (town == null || town.isEmpty()) {
+            this.town = "Информация не указана!";
+        }else {
+            this.town = town;
+        }
+        if  (jobTitle == null || jobTitle.isEmpty()) {
+            this.jobTitle = "Информация не указана!";
+        }else {
+            this.jobTitle = jobTitle;
+        }
     }
 }
